145ee959ea8c1461a2ba2740328f29025ef50c0c
darshanjain533 <darshanjain533@gmail.com>
2022-09-30 17:28:18 +0530

    partial commit done.

--- spring-cloud-config-service/src/main/java/com/djain/microservice/SpringCloudConfigServiceApplication.java
+++ spring-cloud-config-service/src/main/java/com/djain/microservice/SpringCloudConfigServiceApplication.java
@@ -3,11 +3,10 @@ package com.djain.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class SpringCloudConfigServiceApplication {
 
 	public static void main(String[] args) {
